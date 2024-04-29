a2c9d7ad268656fed6451088b2445db5fa671d9a
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-04-29 17:12:04 +0300

    pixel matrix

--- asciiart.py
+++ asciiart.py
@@ -1,5 +1,24 @@
 from PIL import Image
 
-image = Image.open('ascii-pineapple.jpg')
 print('Successfully loaded image')
-print("Image Size:", image.height, 'X', image.width)
+
+def read_pixels_to_array(filepath):
+
+    image = Image.open(filepath)
+
+    height, width = image.size
+
+    mode = image.mode
+
+    pixels = [[None] * width for _ in range(height)]
+    for y in range(height):
+        for x in range(width):
+            pixel_value = image.getpixel((x, y))
+            pixels[y][x] = pixel_value
+
+    return pixels        
+ 
+filepath = 'ascii-pineapple.jpg'
+print_array = read_pixels_to_array(filepath)
+
+print(print_array)
