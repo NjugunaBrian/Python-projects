c64348d8cc33d6e80f99e6dca95fc4f097ad9c51
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-04-30 17:24:32 +0300

    convert to ASCII characters

--- asciiart.py
+++ asciiart.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
-print('Successfully loaded image')
+ASCII_characters = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
 
 def read_pixels_to_array(filepath):
 
@@ -25,9 +26,9 @@ def read_pixels_to_brightness(pixelMatrix, algo_name="average"):
             B = pixelMatrix[y][x][2]
 
             if algo_name == 'average':
-                intensity = (R + G + B) / 3.0
+                intensity = (R + G + B) / 3
             elif algo_name == 'max_min':
-                intensity = (max(pixelMatrix) + min(pixelMatrix) / 2.0)
+                intensity = (max(pixelMatrix) + min(pixelMatrix) / 2)
 
             elif algo_name == 'luminosity':
                 intensity = 0.21*R + 0.72*G + 0.07*B
@@ -41,14 +42,30 @@ def read_pixels_to_brightness(pixelMatrix, algo_name="average"):
 
     return intensity_matrix            
 
+def convert_to_ascii(intensity_matrix, ascii_characters):
+
+    ascii_matrix = []
+    for row in intensity_matrix:
+        ascii_row = []
+        for p in row:
+            index = ascii_characters[int(p/255 * len(ascii_characters)) - 1]
+            ascii_row.append(index)
+        ascii_matrix.append(ascii_row)    
+
+    return ascii_matrix
 
 
-print("Successfully constructed brightness matrix!")
+
+print("Successfully constructed ASCII matrix!")
 
 
 
 filepath = 'ascii-pineapple.jpg'
 print_array = read_pixels_to_array(filepath)
-print_average = read_pixels_to_brightness(print_array, algo_name='luminosity')
-print("Iterating through pixel brightnesses:", print_average)
+print_average = read_pixels_to_brightness(print_array, algo_name='average')
+#print("Iterating through pixel brightnesses:", print_average)
+ascii_representation  = convert_to_ascii(print_average, ASCII_characters)
+print("Iterating through pixel ASCII characters:", ascii_representation)
 
+#I am representing 255 characters onto 70 characters of the ACII character matrix.
+#For each number, I map it onto the appropriate character
