cbf807730c9a100097470d98976f021286e9d92b
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-05-07 18:16:02 +0300

    error handling

--- tictactoe.py
+++ tictactoe.py
@@ -14,7 +14,7 @@ def render(playBoard):
         row_str = f"{i}| " + " | ".join([" " if playBoard[j][i] is None else str(playBoard[j][i] ) for j  in range(3)]) + " |"
         print(row_str)
         print(" +---+---+---+")
-    #print(playBoard); 
+    print(playBoard); 
 
 def get_move():
     print("What is your move's X co-ordinate?")
@@ -35,12 +35,41 @@ def get_move():
             break
 
     move_coords = (X_move, Y_move)
-    print(move_coords)
+    return(move_coords)
 
 
 #board[0][1] = 'X'
 #board[1][1] = 'O' 
 
-render(board)
-print(get_move())
+def make_move(board, move_coords, move):
+    X, Y = move_coords
+
+    new_board = [row[:] for row in board]
+    new_board[X][Y] = move
     
+    if is_valid_move(new_board, move_coords):
+        return new_board
+    else:
+        print(f"Exception: 'Can't make move {(X, Y)}, square already taken!'")
+    
+    
+def is_valid_move(board, move_coords):
+    move_coords = None
+    while True: 
+        move_coords = get_move()
+        X, Y = move_coords
+        if 0 <= X < len(board) and 0 <= Y < len(board[0]):
+            return board[X][Y] is None
+
+        else:
+            print("Invalid move, try again")
+
+
+render(board)
+#get_move()
+move_coords = get_move()
+#make_move(board, move_coords_1, 'X')
+board = make_move(board, move_coords, "X")
+render(board)
+board = make_move(board, move_coords, "X")
+render(board)    
