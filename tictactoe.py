8b50ba8eb740117e82471af1f8f4912ca545bf49
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-05-09 15:46:06 +0300

    alternate moves

--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,6 @@ def new_Board():
     return [[None for _ in range(3)] for _ in range(3)]
 
 
-board = new_Board()
 
 def render(playBoard):
     #print row headers
@@ -41,35 +40,53 @@ def get_move():
 #board[1][1] = 'O' 
 
 def make_move(board, move_coords, move):
+    
+    
     X, Y = move_coords
 
     new_board = [row[:] for row in board]
     new_board[X][Y] = move
     
-    if is_valid_move(new_board, move_coords):
-        return new_board
+    if 0 <= X < len(new_board) and 0 <= Y < len(new_board[0]):
+        if new_board[X][Y] is None:
+            return new_board[X][Y]
     else:
         print(f"Exception: 'Can't make move {(X, Y)}, square already taken!'")
+
+
+    return new_board
     
-    
-def is_valid_move(board, move_coords):
-    move_coords = None
+def is_board_full(board):
+    for col in board:
+        for sq in col:
+            if sq is None:
+                return False
+            
+    return True
+
+def main():
+    board = new_Board()
+    render(board)
+    players = ['X', 'O']
+
+    current_player_index = 0
     while True:
+        current_player_id = players[current_player_index % 2]
         move_coords = get_move()
-        X, Y = move_coords
-        if 0 <= X < len(board) and 0 <= Y < len(board[0]):
-            return board[X][Y] is None
-
-        else:
-            print("Invalid move, try again")
-
-
+        board = make_move(board, move_coords, current_player_id)
+        render(board)
+        current_player_index += 1
+        while not is_board_full(board):
+            current_player_id = players[current_player_index % 2]
+            move_coords = get_move()
+            board = make_move(board, move_coords, current_player_id)
+            render(board)
+            current_player_index += 1
+        
     
-render(board)
-get_move()
-move_coords = get_move()
-board = make_move(board, move_coords, "X")
-render(board)
+if __name__ == '__main__':
+    main()
+
 
 
 
